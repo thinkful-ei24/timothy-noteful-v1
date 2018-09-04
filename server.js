00b62c44763f048d3bde37bde9a20b64ae325ec7
Timothy <superfluous11@gmail.com>
2018-09-04 12:03:29 -0700

    Created GET note details endpoint

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get('/api/notes', (req, res) => {
     res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+    res.json(data.find(note => note.id === Number(req.params.id)));
+});
 
 app.listen(8080, function () {
     console.info(`Server listening on ${this.address().port}`);
