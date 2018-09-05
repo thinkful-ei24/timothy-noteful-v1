f734c439ab7c33ceb70a90881a05b4bc5ff50de4
Timothy <superfluous11@gmail.com>
2018-09-05 11:47:23 -0700

    Created error-handling middleware

--- server.js
+++ server.js
@@ -25,6 +25,24 @@ app.get('/api/notes/:id', (req, res) => {
     res.json(data.find(note => note.id === Number(req.params.id)));
 });
 
+app.get('/boom', (req, res, next) => {
+    throw new Error('Boom!!');
+  });
+
+app.use(function(req, res, next){
+    const err = new Error('Not found');
+    err.status = 404;
+    res.status(404).json({ message: 'Not Found'});
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
+
 app.listen(PORT, function () {
     console.info(`Server listening on ${this.address().port}`);
   }).on('error', err => {
