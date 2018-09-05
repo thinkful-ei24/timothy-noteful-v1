d82f4409300e1d48507eedc85fb6b335fbfac2eb
Timothy <superfluous11@gmail.com>
2018-09-05 12:32:23 -0700

    Updated client

--- public/scripts/noteful.js
+++ public/scripts/noteful.js
@@ -68,8 +68,16 @@ const noteful = (function () {
   function handleNoteFormSubmit() {
     $('.js-note-edit-form').on('submit', function (event) {
       event.preventDefault();
-
-      console.log('Submit Note, coming soon...');
+      const editForm = $(event.currentTarget);
+      const noteObj = {
+        title: editForm.find('.js-note-title-entry').val(),
+        content: editForm.find('.js-note-content-entry').val()
+      };
+      noteObj.id = store.currentNote.id;
+      api.update(noteObj.id, noteObj, updateResponse => {
+        store.currentNote = updateResponse;
+        render();
+      });
 
     });
   }
