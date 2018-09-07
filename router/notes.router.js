bdfab96abf31b3282fb95cd4f612468d711f7f30
Timothy <superfluous11@gmail.com>
2018-09-07 12:16:06 -0700

    Updated POST, PUT and DELETE endpoints with promise implementations

--- router/notes.router.js
+++ router/notes.router.js
@@ -44,15 +44,21 @@ notesRouter.put('/notes/:id', (req, res) => {
         if(field in req.body) updateObj[field] = req.body[field]
     });
 
-    notes.update(id, updateObj, (err, note) => {
+    notes.update(id, updateObj)
+        .then(note => {
+            if(note) res.json(note)
+            else next();
+        });
+
+    /*notes.update(id, updateObj, (err, note) => {
         if(err) next(err);
         if(note) res.json(note);
         else next();
     });
-
+    */
 });
 
-notesRouter.post('/notes', (req, res) => {
+notesRouter.post('/notes', (req, res, next) => {
     const { title, content } = req.body;
     const newItem = { title, content};
     
@@ -62,24 +68,34 @@ notesRouter.post('/notes', (req, res) => {
         return next(err);
     }
 
-    notes.create(newItem, (err, note) => {
+    notes.create(newItem)
+        .then(note => {
+            if(note) res.location(`http://${req.headers.host}/notes/${note.id}`).status(201).json(note);
+            else next();
+        })
+        .catch(err => next(err));
+
+    /* notes.create(newItem, (err, note) => {
         if(err) return next(err);
 
         if(note) res.location(`http://${req.headers.host}/notes/${note.id}`).status(201).json(note);
         else next();
-    });
-
+    }); */
 });
 
-notesRouter.delete('/notes/:id', (req, res) => {
+notesRouter.delete('/notes/:id', (req, res, next) => {
     const id = Number(req.params.id);
-    notes.delete(id, err => {
+    notes.delete(id)
+        .then(() => res.sendStatus(204))
+        .catch(err => next(err));
+    /*notes.delete(id, err => {
         if(err) {
             return next(err);
         } else {
             res.status(204).end();
         }
     });
+    */
 });
 
 module.exports = notesRouter;
